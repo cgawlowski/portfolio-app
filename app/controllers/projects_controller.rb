ef376fa1286e89710525a2d77c5a6f90df4b069e
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2023-01-21 17:17:28 -0500

    CRUD setup

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+    authorize @project
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to root_path(@project), notice: "Le nouveau projet a bien été créé"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to project_path(@project), notice: "Le projet a bien été mis à jour"
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path(@project), notice: "Le projet a bien été supprimé"
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :is_home_item, :is_woodworking, :is_web, photos: [])
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+    authorize @project
+  end
+
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+  has_many_attached :photos
 end
